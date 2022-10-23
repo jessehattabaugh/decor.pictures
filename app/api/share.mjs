196b4e9276400381d9bd7eefdc3354f257b537e4
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2022-10-23 09:46:00 -0700

    uploads a file to S3

--- app/api/share.mjs
+++ app/api/share.mjs
@@ -1,8 +1,22 @@
+import aws from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
 export async function post(req) {
-	const { body } = req;
-	console.log('🧜‍♀️', body, process.env);
-	// upload file to S3
-	return {
-		location: '/thanks',
-	};
+	const s3Client = new aws.S3();
+	const { ARC_STORAGE_PUBLIC_DECOR_PICTURES: Bucket = 'oops' } = process.env;
+	const { base64: Body } = req.body;
+	const Key = uuid();
+	console.log('🪣', Bucket, Body, Key, req);
+	const data = await new Promise((resolve, reject) => {
+		s3Client.putObject({ Body, Bucket, Key }, (err, data) => {
+			if (err) {
+				console.error(err, err.stack);
+				reject(err);
+			} else {
+				console.log('🦄', data);
+				resolve(data);
+			}
+		});
+	});
+	return { data, location: '/thanks' };
 }
